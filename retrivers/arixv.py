9954552780423e6d5ee9cc5b3fc4cdaaddea7eb6
tajbaba999 <tajbaba153@gmail.com>
2026-07-28 22:57:12 +0530

    feat: implement ArxivRetriever for fetching

---
+++ retrivers/arixv.py
@@ -0,0 +1,15 @@
+from langchain_community.retrievers import ArxivRetriever
+
+
+retriever = ArxivRetriever(
+    load_max_docs=2,
+    load_all_available_meta=True
+)
+
+docs = retriever.invoke("large language models")
+
+for i, doc in enumerate(docs):
+    print(f"\nResult - {i+1} :")
+    print("Title :", doc.metadata["Title"])
+    print("Authors :", doc.metadata["Authors"])
+    print("Sumarry :",doc.page_content)

5f8530f3046c0f58831b6438a0bc62069579fe2e
tajbaba999 <tajbaba153@gmail.com>
2026-07-29 20:35:34 +0530

    feat: implement standard method runnable with retry logic

---
+++ runnables/standredmethodexmaplrunnable.py
@@ -0,0 +1,26 @@
+from langchain_core.runnables import RunnableLambda
+
+import random
+
+def add_one(x : int) -> int:
+    return x + 1
+
+def bubby_double(y: int) -> int:
+    """Buggy code that will fail 70% of the time"""
+    if random.random() > 0.3:
+        print('This code failed, and will probably be retried!')
+        raise ValueError('Triggered buggy code')
+    return y * 2
+
+
+sequene = (
+    RunnableLambda(add_one) |
+    RunnableLambda(bubby_double).with_retry(
+        stop_after_attempt=2,
+        wait_exponential_jitter=False
+    )
+)
+
+print(sequene.input_schema.model_json_schema)
+print(sequene.output_schema.model_json_schema)
+print(sequene.invoke(2))

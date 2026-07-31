b13f293ca99bd5c18a534fcb8a8babe9bbc7a83b
tajbaba999 <tajbaba153@gmail.com>
2026-07-31 23:08:03 +0530

    feat: update parallel runnable implementation to include detailed prompt handling

--- runnables/parallelrunnable.py
+++ runnables/parallelrunnable.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
 
 
 load_dotenv()
@@ -19,14 +20,24 @@ detailed_prompt = ChatPromptTemplate.from_template(
 
 topic = "Machine learning"
 
-chain  = {
-   "short" : short_prompt | model | parser,
-   "detailed" : detailed_prompt | model | parser
-}
+# chain  = {
+#    "short" : short_prompt | model | parser,
+#    "detailed" : detailed_prompt | model | parser
+# }
 
-chain.invoke({"topic" : "Machine Learning"})
+# result = chain.invoke({"topic" : "Machine Learning"})
 
+# print(result)
 
+chain = RunnableParallel({
+    "short" : short_prompt | model | parser,
+    "detaield" : detailed_prompt | model | parser
+})
+
+result = chain.invoke({"topic" : "Machine Learnings"})
+
+print(f"Short prompt answer : {result['short']}\n")
+print(f"Long prompt answer : {result['detaield']}")
 
 
 

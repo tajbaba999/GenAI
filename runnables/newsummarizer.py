47202642582fa1e5cf95b175fd5e089eaf0e4555
tajbaba999 <tajbaba153@gmail.com>
2026-08-01 09:59:10 +0530

    feat: implement code generation and explanation functionality in parallel runnable

---
+++ runnables/newsummarizer.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
--- runnables/parallelrunnable.py
+++ runnables/parallelrunnable.py
@@ -10,15 +10,15 @@ load_dotenv()
 model = ChatMistralAI(model="mistral-small-2506")
 parser = StrOutputParser()
 
-short_prompt = ChatPromptTemplate.from_template(
-    "Explain {topic} in 1-2 lines"
-)
+# short_prompt = ChatPromptTemplate.from_template(
+#     "Explain {topic} in 1-2 lines"
+# )
 
-detailed_prompt = ChatPromptTemplate.from_template(
-    "Explain the concept in detail"
-)
+# detailed_prompt = ChatPromptTemplate.from_template(
+#     "Explain the concept in detail"
+# )
 
-topic = "Machine learning"
+# topic = "Machine learning"
 
 # chain  = {
 #    "short" : short_prompt | model | parser,
@@ -29,15 +29,40 @@ topic = "Machine learning"
 
 # print(result)
 
-chain = RunnableParallel({
-    "short" : RunnableLambda(lambda x: {'topic': x['topic']}) | short_prompt | model | parser,
-    "detailed" : RunnableLambda(lambda x: {'topic': x['topic']}) | detailed_prompt | model | parser
+# chain = RunnableParallel({
+#     "short" : RunnableLambda(lambda x: {'topic': x['topic']}) | short_prompt | model | parser,
+#     "detailed" : RunnableLambda(lambda x: {'topic': x['topic']}) | detailed_prompt | model | parser
+# })
+
+# result = chain.invoke({"topic" : "Machine Learnings"})
+
+# print(f"Short prompt answer : {result['short']}\n")
+# print(f"Long prompt answer : {result['detailed']}")
+
+
+code_prompt = ChatPromptTemplate.from_messages({
+    ("system", "You are a code genrator"),
+    ("human", "{topic}")
 })
 
-result = chain.invoke({"topic" : "Machine Learnings"})
+explain_prompt = ChatPromptTemplate.from_messages({
+    ("system", "You are a helpful coding assitent who explain coding in simple terms"),
+     ("human", "Explain the following code in simple words:\n{code}")
+})
 
-print(f"Short prompt answer : {result['short']}\n")
-print(f"Long prompt answer : {result['detailed']}")
+seq = code_prompt | model | parser
 
+seq2 = RunnableParallel(
+    {"code" : RunnableParallel(),
+    "explanation" : explain_prompt | model | parser
+    }
+  )
+
+chain = seq | seq2
+
+result = chain.invoke({"topic" : "please write a code of palindrome in python"})
+
+print(result['code'])
+print(result['explanation'])
 
 

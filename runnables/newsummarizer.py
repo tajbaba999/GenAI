8ce6708bbaf2a0fb4cc195507ce1b9297d79ac07
tajbaba999 <tajbaba153@gmail.com>
2026-08-01 10:14:10 +0530

    feat: enhance news summarization by integrating TavilySearch and updating prompt structure

--- runnables/newsummarizer.py
+++ runnables/newsummarizer.py
@@ -1,8 +1,30 @@
 from dotenv import load_dotenv
 load_dotenv()
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_tavily import TavilySearch
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
+search_tool = TavilySearch(max_result=5)
+
+
+llm = ChatMistralAI(model = "mistral-small-2506")
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    You are the helpful assistant
+
+    summarize the follwing into clear bullet points.
+    {news}
+    """
+)
+
+chain = prompt | llm | StrOutputParser()
+
+news_result = search_tool.run("Latest ai tool of 2026")
+
+result = chain.invoke({"news" : news_result})
+
+print(result)
 
 
